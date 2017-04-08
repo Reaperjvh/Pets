fb10ed75536e62f291df23c21f7dc44ba04a281e
jvh <jvh@afrihost.co.za>
2017-04-08 22:32:18 +0200

    1.8 Add constants for pet content URIs

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,5 +1,6 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -8,11 +9,19 @@ import android.provider.BaseColumns;
 
 public final class PetContract {
 
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_PETS = "pets";
+
     // To prevent someone from accidentally instantiating the contract class,
     // give it an empty constructor
     private PetContract() {}
 
     public static final class PetEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
         public static final String TABLE_NAME = "pets";
 
