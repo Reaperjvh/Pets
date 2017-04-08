8cde65363d5cb32f06d7d79b2a2dcdb9c5cf7522
jvh <jvh@afrihost.co.za>
2017-04-08 22:58:37 +0200

    1.9 Add UriMatcher to ContentProvider

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -2,6 +2,7 @@ package com.example.android.pets.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -12,6 +13,25 @@ import android.support.annotation.Nullable;
  */
 
 public class PetProvider extends ContentProvider{
+
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    private static final int PETS = 100;
+
+    private static final int PET_ID = 101;
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        // The content URI of the form "content://com.example.android.pets/pets" will map to the
+        // integer code {@link #PETS}. This URI is used to provide access to MULTIPLE rows
+        // of the pets table.
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS, PETS);
+
+        // The content URI of the form "content://com.example.android.pets/# will map to the
+        // integer code {@link #PET_ID}. This URI is used to provide access to a single row
+        // of the pets table.
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS + "/#", PET_ID);
+    }
 
     /** Database helper object */
     private PetDbHelper mDbHelper;
