fc77ff0aa717dafd9c26334ec512837772335535
jvh <jvh@afrihost.co.za>
2017-04-08 23:12:04 +0200

    1.10 Implement ContentProvider query() method

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -1,12 +1,15 @@
 package com.example.android.pets.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import com.example.android.pets.data.PetContract.PetEntry;
 
 /**
  * Created by jvh on 4/8/2017.
@@ -43,8 +46,31 @@ public class PetProvider extends ContentProvider{
     }
 
     @Override
-    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
-        return null;
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder) {
+        // Get readable database
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        // This Cursor will hold the result of the query/
+        Cursor cursor;
+
+        // Figure out if the UriMatcher can match the URI to a specific code
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                cursor = db.query(PetEntry.TABLE_NAME, projection,selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            case PET_ID:
+                selection = PetEntry._ID + "=?";
+                selectionArgs = new String[] {String.valueOf(ContentUris.parseId(uri))};
+                cursor = db.query(PetEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+        return cursor;
     }
 
     @Nullable
